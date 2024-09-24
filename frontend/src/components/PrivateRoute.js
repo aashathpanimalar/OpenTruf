34fd4c0d8081af8764c8601a4275f3ed79c66ccf
aashathpanimalar <135994661+aashathpanimalar@users.noreply.github.com>
2024-09-24 12:35:47 +0530

    Update PrivateRoute.js

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Route } from 'react-router-dom';
 
 const PrivateRoute = ({ element: Component, ...rest }) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('loggedInUser');
 
   return token ? <Component {...rest} /> : <Navigate to="/" />;
 };
