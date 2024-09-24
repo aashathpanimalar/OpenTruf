4a40fcdf03bc588d0526855309f7631c998635ff
aashathpanimalar <135994661+aashathpanimalar@users.noreply.github.com>
2024-09-24 12:30:05 +0530

    Create PrivateRoute.js

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navigate, Route } from 'react-router-dom';
+
+const PrivateRoute = ({ element: Component, ...rest }) => {
+  const token = localStorage.getItem('token');
+
+  return token ? <Component {...rest} /> : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
